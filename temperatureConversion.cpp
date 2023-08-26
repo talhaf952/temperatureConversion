aaf2388bf29522f69880a4a6983a841e97faf9b8
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-08-26 23:02:34 +0500

    Kelvin

--- temperatureConversion.cpp
+++ temperatureConversion.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main()
 {
-	float fahrenheit, celsius;
+	float fahrenheit, celsius,Kelvin;
 	cout<<"Enter Temperature in Celsius: ";
 	cin>>celsius;
 	fahrenheit = (celsius * 9.0) / 5.0 + 32;
+	Kelvin= celsius + 273.15;
 	cout<<"The Temperature in Celsius: "<<celsius<<endl;
 	cout<<"The Temperature in Fahrenheit: "<<fahrenheit<<endl;
+	cout<<"The Temperature in Kelvin: "<<Kelvin<<endl;
 	return 0;
 }
