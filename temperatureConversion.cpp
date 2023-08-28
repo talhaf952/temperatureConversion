bb22600f9ecc0c9a9e80c82e52eaffd4e681a952
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-08-28 10:31:02 +0500

    Ask to continue or not

--- temperatureConversion.cpp
+++ temperatureConversion.cpp
@@ -2,42 +2,53 @@
 using namespace std;
 
 int main() {
-    float fahrenheit, celsius, Kelvin;
-    char unit;
+    char continueProgram = 'y';
 
-    cout << "In which unit you want to enter temperature:\n";
-    cout << "For Celsius, type 'c' or 'C'.\n";
-    cout << "For Fahrenheit, type 'f' or 'F'.\n";
-    cout << "For Kelvin, type 'k' or 'K': ";
-    cin >> unit;
+    while (continueProgram == 'y' || continueProgram == 'Y') {
+        float fahrenheit, celsius, Kelvin;
+        char unit;
 
-    if (unit == 'c' || unit == 'C') {
-        cout << "Enter Temperature in Celsius: ";
-        cin >> celsius;
-        fahrenheit = (celsius * 9.0) / 5.0 + 32;
-        Kelvin = celsius + 273.15;
-        cout << "The Temperature in Celsius: " << celsius << endl;
-        cout << "The Temperature in Fahrenheit: " << fahrenheit << endl;
-        cout << "The Temperature in Kelvin: " << Kelvin << endl;
-    } else if (unit == 'f' || unit == 'F') {
-        cout << "Enter Temperature in Fahrenheit: ";
-        cin >> fahrenheit;
-        celsius = (fahrenheit - 32) * 5.0 / 9.0;
-        Kelvin = celsius + 273.15;
-        cout << "The Temperature in Celsius: " << celsius << endl;
-        cout << "The Temperature in Fahrenheit: " << fahrenheit << endl;
-        cout << "The Temperature in Kelvin: " << Kelvin << endl;
-    } else if (unit == 'k' || unit == 'K') {
-        cout << "Enter Temperature in Kelvin: ";
-        cin >> Kelvin;
-        celsius = Kelvin - 273.15;
-        fahrenheit = (celsius * 9.0) / 5.0 + 32;
-        cout << "The Temperature in Celsius: " << celsius << endl;
-        cout << "The Temperature in Fahrenheit: " << fahrenheit << endl;
-        cout << "The Temperature in Kelvin: " << Kelvin << endl;
-    } else {
-        cout << "Wrong Input, Try again." << endl;
+        cout << "In which unit you want to enter temperature:\n";
+        cout << "For Celsius, type 'c' or 'C'.\n";
+        cout << "For Fahrenheit, type 'f' or 'F'.\n";
+        cout << "For Kelvin, type 'k' or 'K': ";
+        cin >> unit;
+
+        if (unit == 'c' || unit == 'C') {
+            cout << "Enter Temperature in Celsius: ";
+            cin >> celsius;
+            fahrenheit = (celsius * 9.0) / 5.0 + 32;
+            Kelvin = celsius + 273.15;
+            cout << "The Temperature in Celsius: " << celsius << endl;
+            cout << "The Temperature in Fahrenheit: " << fahrenheit << endl;
+            cout << "The Temperature in Kelvin: " << Kelvin << endl;
+        } else if (unit == 'f' || unit == 'F') {
+            cout << "Enter Temperature in Fahrenheit: ";
+            cin >> fahrenheit;
+            celsius = (fahrenheit - 32) * 5.0 / 9.0;
+            Kelvin = celsius + 273.15;
+            cout << "The Temperature in Celsius: " << celsius << endl;
+            cout << "The Temperature in Fahrenheit: " << fahrenheit << endl;
+            cout << "The Temperature in Kelvin: " << Kelvin << endl;
+        } else if (unit == 'k' || unit == 'K') {
+            cout << "Enter Temperature in Kelvin: ";
+            cin >> Kelvin;
+            celsius = Kelvin - 273.15;
+            fahrenheit = (celsius * 9.0) / 5.0 + 32;
+            cout << "The Temperature in Celsius: " << celsius << endl;
+            cout << "The Temperature in Fahrenheit: " << fahrenheit << endl;
+            cout << "The Temperature in Kelvin: " << Kelvin << endl;
+        } else {
+            cout << "Wrong Input, Try again." << endl;
+            cout << "Do you want to continue? (y/n): ";
+            cin >> continueProgram;
+            continue;
+        }
+
+        cout << "Do you want to continue? (y/n): ";
+        cin >> continueProgram;
     }
+
 
     return 0;
 }
